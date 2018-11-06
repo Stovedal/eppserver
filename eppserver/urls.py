281da036400f78d7cf62a7480d40981852622d33
Sofia Tovedal <sofia.tovedal@gmail.com>
2018-11-06 11:19:21 +0100

    added views

--- eppserver/urls.py
+++ eppserver/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.urls import include, path
 from django.contrib import admin
+from users import urls
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('users/', include('users.urls')),
 ]
